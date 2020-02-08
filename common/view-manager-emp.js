263745958725dfa5a98e286a07d6740d2b1654e3
Marco Carrillo <marco_carrillo@yahoo.com>
2020-02-07 22:59:45 -0500

    Fixed SQL bug for viewing all employees by manager

--- common/view-manager-emp.js
+++ common/view-manager-emp.js
@@ -5,10 +5,11 @@
 let budget_view=require('./table-view');
 
 module.exports = async function view_manager(connection,inquirer){
-sql=`SELECT CONCAT(m.first_name,' ',m.last_name) AS Manager,e.id, e.first_name, e.last_name  `+
+sql=`SELECT CONCAT(m.first_name,' ',m.last_name) AS Manager,e.id, e.first_name, e.last_name,r.title,r.salary,d.name  `+
             'FROM employee e '+
             'LEFT JOIN employee m on m.id=e.manager_id '+
-            'GROUP BY manager '+
+            'LEFT JOIN role r on r.id=e.role_id '+
+			'LEFT JOIN department d on d.id=r.department_id '+
             'ORDER BY manager;'
     
     db_name='employees'
--- employee/employee-view.js
+++ employee/employee-view.js
@@ -6,8 +6,9 @@ let table_view=require('../common/table-view');
 
 module.exports = async function view_roles(connection,inquirer){
 
-sql='SELECT ee.id,ee.first_name,ee.last_name,rl.title,rl.salary,dp.name as "Department name" '+ 
+sql=`SELECT ee.id,ee.first_name,ee.last_name,rl.title,rl.salary,dp.name as "Department name",CONCAT(ee2.first_name,' ',ee2.last_name) as "Manager Name"`+ 
             'FROM employee ee ' +
+            'LEFT JOIN employee ee2 on ee2.id=ee.manager_id ' +
             'LEFT JOIN role rl ON rl.id=ee.role_id '+
             'LEFT JOIN department dp ON dp.id=rl.department_id '+
             'ORDER BY ee.id ASC;'
